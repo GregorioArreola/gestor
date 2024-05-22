828570b444515389a9c61bb844c1bc5ba1903a24
GregorioArreola <naborgregorio02@gmail.com>
2024-05-22 11:30:53 -0600

    Antes de instalar winnmp otra vez
    reinicare winnmp

--- views/viewsAdminLte/site/index.php
+++ views/viewsAdminLte/site/index.php
@@ -1,35 +1,48 @@
 <?php
 $this->title = 'Inicio';
-$this->params['breadcrumbs'] = [['label' => $this->title]];
+$this->params['breadcrumbs'][] = $this->title;
+
 ?>
 <div class="container-fluid">
     <div class="row">
-
         <div class="col-md-3 col-sm-6 col-12">
             <?= \hail812\adminlte\widgets\InfoBox::widget([
                 'text' => 'Tareas recientes',
                 'theme' => 'gradient-success',
-
             ]) ?>
+            <ul>
+                <?php if (!empty($tareasRecientes)) : ?>
+                    <?php foreach ($tareasRecientes as $tarea) : ?>
+                        <li><?= $tarea->tar_nombre ?></li>
+                    <?php endforeach; ?>
+                <?php else : ?>
+                    <li>No hay tareas recientes.</li>
+                <?php endif; ?>
+            </ul>
         </div>
-
-    </div>
-
-    <div class="row">
         <div class="col-md-3 col-sm-6 col-12">
             <?= \hail812\adminlte\widgets\InfoBox::widget([
                 'text' => 'Tareas vencidas',
                 'theme' => 'gradient-danger',
             ]) ?>
+<ul>
+                <?php if (!empty($tareasVencidas)) : ?>
+                    <?php foreach ($tareasRecientes as $tarea) : ?>
+                        <li><?= $tarea->tar_nombre ?></li>
+                    <?php endforeach; ?>
+                <?php else : ?>
+                    <li>No hay tareas recientes.</li>
+                <?php endif; ?>
+            </ul>
         </div>
+
         <div class="col-md-3 col-sm-6 col-12">
             <?= \hail812\adminlte\widgets\InfoBox::widget([
-                'text' => 'Tareas proximas a vencer',
+                'text' => 'Tareas próximas a vencer',
                 'theme' => 'gradient-warning',
             ]) ?>
+
+
         </div>
     </div>
-
-</div>
-</div>
 </div>
