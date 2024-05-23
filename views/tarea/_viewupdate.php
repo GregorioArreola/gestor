0f663e5bcc0a7f58c05ebfebdd3555e558720c18
Jesus Adrian <jesusadrianjeronimo113@gmail.com>
2024-05-22 23:44:54 -0600

    Reapply "views"
    This reverts commit bbea0db6cd14e4b933de42a308978cf1d3af230d.

--- views/tarea/_viewupdate.php
+++ views/tarea/_viewupdate.php
@@ -31,6 +31,7 @@ $this->registerJs("
 ?>
 
 <div class="tarea-form">
+
     <?php $form = ActiveForm::begin(); ?>
 
     <div class="row">
@@ -38,39 +39,39 @@ $this->registerJs("
             <?= $form->field($model, 'tar_nombre')->textInput(['maxlength' => true]) ?>
         </div>
         <div class="col-md-6">
-            <?= $form->field($model, 'tar_descripcion')->textInput(['maxlength' => true, 'class' => 'form-control form-control-lg']) ?>
-        </div>
+    <?= $form->field($model, 'tar_descripcion')->textarea(['maxlength' => true, 'class' => 'form-control form-control-lg']) ?>
+</div>
+
     </div>
 
     <div class="row">
-<<<<<<< HEAD
-=======
         <div class="col-md-4">
         <?= $form->field($model, 'tar_fkprioridad')->widget(Select2::class, [
                 'data' => ArrayHelper::map(Prioridad::find()->all(), 'prio_id', 'prio_nombre'),
-                'options' => ['placeholder' => 'Seleccione la prioridad...', 'id' => 'prioridad-select'],
+                'options' => ['placeholder' => 'tar_fkprioridad', 'id' => 'prioridad-select'],
                 'pluginOptions' => ['allowClear' => true],
             ]) ?>
         </div>
         <div class="col-md-4">
             <?= $form->field($model2, 'mat_fkperiodo')->widget(Select2::class, [
                 'data' => ArrayHelper::map(Periodo::find()->all(), 'per_id', 'per_nombre'),
-                'options' => ['placeholder' => 'Selecciona un periodo...', 'id' => 'periodo-select'],
+                'options' => ['placeholder' => 'Cambiar periodo', 'id' => 'periodo-select'],
                 'pluginOptions' => ['allowClear' => true],
             ]) ?>
         </div>
         <div class="col-md-4">
             <?= $form->field($model, 'tar_fkmateria')->widget(Select2::class, [
-                'data' => [],
-                'options' => ['placeholder' => 'Primero selecciona un periodo'],
+                'data' => [ ArrayHelper::map(Materia::find()->all(), 'mat_id', 'mat_nombre'),],
+                'options' => ['placeholder' => 'mat_nombre'],
                 'pluginOptions' => ['allowClear' => true],
             ]) ?>
         </div>
     </div>
 
     <div class="row">
-        <div class="col-md-3">
-            <?= $form->field($model, 'tar_creacion')->widget(DatePicker::class, [
+       
+        <div class="col-md-6">
+            <?= $form->field($model, 'tar_inicio')->widget(DatePicker::class, [
                 'options' => ['class' => 'form-control'],
                 'pluginOptions' => [
                     'autoclose' => true,
@@ -90,9 +91,10 @@ $this->registerJs("
     </div>
     
     <div class="form-group">
-        <?= Html::submitButton('Crear', ['class' => 'btn btn-success']) ?>
+    <?= Html::submitButton('Confirmar', ['class' => 'btn btn-success', 'name' => 'redirect-button']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
+
 
 </div>
